acb9f1bf48db48d8f651298b0ff0c71b675e5441
Edgar Gomez <egomez@xtivia.com>
2016-05-18 16:39:34 -0500

    formatting and a comment

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,21 +8,25 @@
  * Controller of the form2fileApp
  */
 angular.module('form2fileApp')
-  .controller('MainCtrl', ['$scope', function ($scope) {
+  .controller('MainCtrl', ['$scope', function ($scope) 
+  {
   	$scope.formData = {};
   	$scope.file = {};
     $scope.processForm = function()
     {
-    	var form2File = new Blob([JSON.stringify($scope.formData)], {type: "text/plain;charset=utf-8"});
+      var form2File = new Blob([JSON.stringify($scope.formData)], {type: "text/plain;charset=utf-8"});
 
-    	if($scope.file.name == undefined){
-    		$scope.file.name = "form2file";
-    	}
+      //give a default filename and extension if none was typed in
+      if($scope.file.name == undefined)
+      {
+        $scope.file.name = "form2file";
+      }
 
-		if($scope.file.extension == undefined){
-    		$scope.file.extension = "txt";
-    	}
-    	
-		saveAs(form2File, $scope.file.name + '.' + $scope.file.extension);
+      if($scope.file.extension == undefined)
+      {
+        $scope.file.extension = "txt";
+      }
+
+      saveAs(form2File, $scope.file.name + '.' + $scope.file.extension);
     };
   }]);
